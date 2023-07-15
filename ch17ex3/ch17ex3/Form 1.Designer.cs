9592445d3c9f0ce74208c02dd846c159a64835f2
Miranda Morris <94880693+Juniperistic@users.noreply.github.com>
2023-07-15 19:55:19 -0400

    Upload of Displaying File in a Textbox

---
+++ ch17ex3/ch17ex3/Form 1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ch17ex3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGrades = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtGrades
+            // 
+            this.txtGrades.BackColor = System.Drawing.SystemColors.Control;
+            this.txtGrades.Location = new System.Drawing.Point(12, 12);
+            this.txtGrades.Multiline = true;
+            this.txtGrades.Name = "txtGrades";
+            this.txtGrades.ReadOnly = true;
+            this.txtGrades.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtGrades.Size = new System.Drawing.Size(364, 207);
+            this.txtGrades.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 231);
+            this.Controls.Add(this.txtGrades);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGrades;
+    }
+}
+
---
+++ ch17ex3/ch17ex3/Form 1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*************************************************************************/
+/* Program Name:     Ch17Ex3.cs                                          */
+/* Date:             05/02/2022                                      	 */
+/* Programmer:       Miranda Morris                                      */
+/* Class:            CSCI 234                               		     */
+/*                                                                       */
+/* Program Description: The purpose of this program is to create a file  */
+/* that contains the name, id number, class taken, and grade of each     */
+/* student within the file and display it into a read-only textbox       */
+/*                                                                       */
+/* Input: grades.txt file containig name, id number, class taken, & grade*/
+/*                                                                       */
+/* Output: display the contents of grade.txt on a textbox                */
+/*                                                                       */
+/* Givens: None							                                 */
+/*                                                                       */
+/* Testing Data:                                                         */
+/*                                                                       */
+/* List the Testing Input Data: Run the program                          */
+/*                                                                       */
+/* List the Testing Output Data: "There was a problem loading the file"  */
+/*                                                                       */
+/*                                                                       */
+/*************************************************************************/
+
+namespace ch17ex3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //C: \Users\miran\Desktop\ch17ex3\ch17ex3\grades.txt
+            //string filePath = @"C:\...\...\...\...\...\...\...\...\grades.txt";
+            
+            string path = @"C:\Users\miran\Desktop\ch17ex3\ch17ex3\grades.txt";
+            //if (!File.Exists(filePath))
+            if (!File.Exists(path))
+            {
+                StreamReader stream = new StreamReader(path);
+                txtGrades.AppendText(stream.ReadToEnd());
+            }
+            else
+            {
+                MessageBox.Show("There was a problem loading the file");
+            }
+        }
+    }
+}
